0d1baaae80a0cc8e85c6e75b999c261b9643bd7e
ekomulyachandra <ekomulyachandra99@gmail.com>
2020-03-27 18:52:22 +0700

    project perfilm-an

---
+++ app/src/main/java/com/example/moviebeta/network/Constant.java
@@ -0,0 +1,13 @@
+package com.example.moviebeta.network;
+
+import com.example.moviebeta.BuildConfig;
+
+public class Constant {
+    public static final String BASE_URL="https://api.themoviedb.org";
+    public static final String IMG_URL="https://image.tmdb.org/t/p/w500";
+    public static final String MOVIE_API_KEY="441268c74f358c7b2d53fe71e85738ab";
+    public static final String VERSION="/3";
+    public static final String MOVIE ="/movie";
+    public static final String LANG_EN ="en_US";
+    public static final String MOVIE_PATH =VERSION+MOVIE;
+}
